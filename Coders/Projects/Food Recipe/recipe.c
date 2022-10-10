ca52ec525af2c6c2b3310e5a7b0b5e484b63a1a1
Mayank Badsara <72911127+BadsaraMayank@users.noreply.github.com>
2022-10-10 17:42:48 +0530

    Create recipe.c

---
+++ Coders/Projects/Food Recipe/recipe.c
@@ -0,0 +1,141 @@
+#include<stdio.h>
+#include<conio.h>
+/* Enter your dish's number mentioned before to get recipe for the same 
+   Made By Mayank Badsara */
+int main(){
+    int number;
+
+                  printf(" 1 : DAAL \n");
+                  printf(" 2 : POTATO AND BROCCOLI CURRY (Vegan) \n");
+                  printf(" 3 : GRILLED CARROT DOGS \n");
+                  printf(" 4 : CHAPATI \n");
+                  printf(" 5 : RICE \n");
+
+        printf("Enter A Number (1-5) : ");
+        scanf("%d", &number);
+
+    switch(number){
+
+    case 1:printf(" \t\t\t---| DAAL RECIPE |---\n\n");
+                
+                 printf("--> Ingredients \n\n");
+                 printf("1 cup red lentils (yellow lentils will work as well)\n");
+                 printf("3 cups water \n");
+                 printf("3 plum tomatoes \n");
+                 printf("2 teaspoons vegetable oil \n");
+                 printf("1/2 cup finely chopped white or yellow onion \n");
+                 printf("2 medium cloves garlic, f inely chopped and made into a paste \n");
+                 printf("2 teaspoons Bengali five spice mix (panch phoron) OR 1/2 teaspoon nigella seeds or black (or white) sesame seeds, 1/2 teaspoon cumin seeds, 1/2 teaspoon fennel seeds, 1/2 teaspoon mustard seeds,  and (if available) 1/2 teaspoon fenugreek seeds \n");
+                 printf("1 bay leaf , 1 teaspoon turmeric \n");
+                 printf("1 teaspoon kosher salt plus more to taste \n");
+                 printf("1 lime, juiced (about 2 tablespoons fresh lime juice) \n");
+                 printf("8 sprigs cilantro, de-stemmed and chopped \n");
+                 printf("  Cooked basmati rice, optional \n");
+                 printf(" \n");
+                 printf(" \n");
+                 
+                 printf("\t\t\t---> Steps <--- \n\n");
+                 printf("-> 1. Cook the lentils: Place 1 cup of red lentils in a metal sieve. Rinse well with cold water..Pour cleaned lentils into a medium sauce pan. Add 3 cups of water. Bring lentils      and water to a boil. Reduce heat, cover and simmer for 10 minutes or until the lentils are soft \n");
+                 printf(" \n");
+                 printf(" \n");
+                 printf("2. Blanch and chop the tomatoes: While the lentils are cooking in step 1, bring a separate small pot of water to a boil. Score the peel of the tomatoes with a sharp knife in the   shape of an X . Place the tomatoes in the boiling water and blanch for one minute. Remove the tomatoes to a bowl to cool. Once cool, peel the tomatoes and cut out and discard   the tough stem end. Chop the tomatoes, or mash them, and set aside \n");
+                 printf(" \n");
+                 printf(" \n");
+                 printf("4.Saute the onions with seasonings: After the lentils in step 1 have cooked at least 5 minutes, start preparing the onions and spices. In a medium saucepan, heat the oil over  medium heat. Add the chopped onions. Cook until translucent, about 3 minutes. Add garlic paste and cook for 1 minute more, stirring continuously, making sure that the garlic    does not burn. Add the Bengali five spice. Cook and stir for another 2-3 minutes. Add bay leaf and turmeric. Stir  \n");
+                 printf(" \n");
+                 printf(" \n");
+                 printf("5. Add cooked lentils: To the onions and spices, add the cooked lentils along with the lentil cooking water. Add salt. Cook for 10 minutes. \n");
+                 printf(" \n");
+                 printf(" \n");
+                 printf("6. Finish the soup: Add lime juice and tomatoes. Cook for 3-5 more minutes. Adjust salt if necessary. Stir in chopped cilantro and remove from heat. Garnish with more chopped cilantro. Serve with basmati rice or naan bread. \n");
+                 printf("Have A Nice Cooking :)");
+           
+        break;
+
+    case 2:printf(" \t\t\t---|POTATO AND BROCCOLI CURRY (Vegan)|--- \n\n");
+                 
+                 printf("--> Ingredients \n\n");
+                 printf("-> 3 large (1 1/4 pounds, 670g) Yukon Gold or other golden potatoes \n");
+                 printf("-> 3 big stalks broccoli with fist-sized crowns, or 1 larger melon-sized crown \n");
+                 printf("-> 1 (2-inch) piece fresh ginger, peeled and coarsely chopped \n");
+                 printf("-> 1 clove garlic, chopped \t -> 1 (14- to 16-ounce) can diced tomatoes \n");
+                 printf("-> 3/4 cup roasted unsalted whole almonds \t -> 1/2 teaspoon salt, or more to taste \n");
+                 printf("-> 2 tablespoons canola oil \t -> 1 teaspoon ground coriander \n");
+                 printf("-> 1 teaspoon ground cumin \t -> 1/2 teaspoon red chili powder \n");
+                 printf("-> 1/2 teaspoon ground turmeric \t -> 3 cups vegetable stock \n");
+                 printf("-> 1 stick cinnamon \t -> 2 tablespoons chopped fresh cilantro, for garnish \n\n");
+                
+                 printf("\t\t\t--> Steps <--\n\n");
+                 printf("1. Prepare the potatoes: Without peeling, cut the potatoes into 1/2-inch cubes. Steam the potatoes over boiling water in a vegetable steamer, tightly covered, for 10 minutes. Drain. When done, the potatoes should be firm, but tender and easily pierced with a fork.\n");
+                 printf("2. Prepare the broccoli: Cut the stems from the crown. Use a paring knife to peel the tough outer skin from the broccoli stems. Slice the stems into 1/4-inch pieces. Cut the crown into florets about 3 inches long.\n");
+                 printf("3. Make the sauce: In a food processor or blender, combine the ginger, garlic, tomatoes, 1/2 cup of the almonds (set aside the remaining 1/4 cup for garnish), and salt. Pulse the mixture until smooth.\n");
+                 printf("4. Cook the spices: In a Dutch oven or other large, heavy pot with a lid, heat the canola oil over medium heat. Add the chile powder, coriander, cumin, and turmeric. Cook, stirring, for 1 minute until the spices are fragrant. \n");
+                 printf("5. Simmer the sauce: Add the puréed tomato mixture to the pot with the spices..stand back as it may splutter as you add it to the pan. Cook, stirring, for 2 minutes.Add the stock and cinnamon stick. Bring to a boil. Simmer uncovered, stirring occasionally, for 10 minutes.\n");
+                 printf("6. Cook the vegetables: Add the potatoes and broccoli to the sauce and bring to a boil. Cover the pot and simmer for 8 minutes, or until all the vegetables are tender when pierced with a skewer.\n");
+                 printf("7. Serve the curry: Remove the cinnamon stick. Coarsely chop the remaining 1/4 cup almonds. Ladle the curry into bowls with rice and garnish with almonds and cilantro before serving. \n");
+                 printf("\t\t\t\tHave A Nice Cooking :)");
+
+
+        break;
+
+    case 3:printf("\t\t\t---|GRILLED CARROT DOGS|---\n\n");
+                 
+                 printf("-> Ingredients \n\n");
+                 printf("-> 6 carrots, about 1-inch in diameter, peeled \t -> 1/4 cup yellow mustard \n");
+                 printf("-> 2 tablespoons Dijon mustard \t -> 1 teaspoon granulated garlic \n");
+                 printf("-> 1 teaspoon paprika \t -> 1/2 teaspoon ground cumin \n");
+                 printf("-> 1/2 teaspoon dry ground mustard, like Coleman's \t -> 6 hot dog buns \n\n");
+                
+                 printf("\t\t\t--> Steps <-- \n\n");
+                 printf("1. Bring a pot of water to a boil over high heat \n");
+                 printf("2. Cut the carrots: While the water comes to a boil, peel and cut the carrots to the length of a hot dog bun approximately 6 inches. Remove parts of the carrot, so the 6-inch portion is relatively uniform in size. If necessary, use a vegetable peeler to remove excess carrot. \n");
+                 printf("3. Parboil the carrots: In a large pot of boiling water, parboil the carrots until knife tender, approximately 10 to 12 minutes. Drain carrots into a colander. With a paper towel, pat the carrots dry and place on a sheet tray or plate. \n");
+                 printf("4. Make the paste: In a small bowl, combine the yellow mustard, Dijon mustard, garlic, paprika, cumin, and ground mustard. \n");
+                 printf("5. Coat the carrots with the paste: Using a spoon, smear the mustard over the carrots, on all sides. \n");
+                 printf("6. Prepare a grill for direct medium heat—350° to 450°F \n");
+                 printf("7. Grill the carrots: Grill the carrots over direct heat, turning several times until marked all over, 6 to 8 minutes total. \n");
+                 printf("8. Grill the hot dog buns: Grill the hot dog buns, cut side down, over direct heat, until marked, 45 to 60 seconds. \n");
+                 printf("9. Serve: Place a carrot in each bun and eat with your favorite toppings. \n");
+                 printf("\t\t\t\tHave A Nice Cooking :)");
+           
+            break;
+
+    case 4:printf("\t\t\t---|CHAPATI|--- \n\n");
+                
+                 printf("-> Ingredients \n\n");
+                 printf("-> 2 cups whole wheat flour or drum-wheat (Atta) \n");
+                 printf("-> 1 teaspoon salt \t -> 1 cup water \n");
+                 printf("-> 2 tablespoons wheat flour, for rolling and dusting \n\n");
+                 
+                 printf("\t\t\t--> Steps <-- \n\n");
+                 printf("2. Knead the flour until it becomes soft and pliable and doesn't stick to your fingers. You can put a little oil on your hand while kneading the dough to help with kneading. Cover the dough with plastic wrap and let the dough rest for at least 1 hour, at room temperature. You may store the dough in refrigerator. Just thaw to room temperature before using it. \n");
+                 printf("3. Once ready to make chapatis, heat the griddle over medium-high heat. Divide the dough into 10-12 equal-sized dough balls. Working on one dough ball at a time. Roll a dough ball in the flour and flatten it a bit with your hands. Transfer the flatten ball to a clean flat surface, roll it with a rolling pin into a 6-7 inch disc. If the dough sticks to the surface, dust the surface with more flour. \n");
+                 printf("4. Place the chapati on the hot griddle and cook for 30 seconds or until tiny golden dots appear on the surface, flip over and cook the other side. Flip over again and soon the chapati will start to puff up. Use a folded kitchen towel and press gently on the puffy chapati to push the air to the flatten part of of the chapati. The whole bread should puff up into a round ball. \n");
+                 printf("5. Transfer the cooked chapatis to a serving platter. You may baste it with a little butter or ghee. Serve immediately. \n");
+                 printf("\t\t\t\tHave A Nice Cooking :)");
+           
+            break;
+
+    case 5:printf("\t\t\t---|RICE|---\n\n");
+                 printf("->Ingredients \n\n");
+                 printf("-> 1 cup long-grain white rice \n");
+                 printf("-> 2 cups water \t -> 1/2 teaspoon salt \n");
+                 printf("-> 1 tablespoon unsalted butter (optional) \n");
+                 printf("-> 1 tablespoon finely chopped parsley (optional) \n");
+          
+                 printf("\t\t\t--> Steps <-- \n\n");
+                 printf("1. Gather the ingredients. \n");
+                 printf("2. Rinse the rice under cold water until the water runs clear. Drain in a colander and set aside. \n");
+                 printf("3. In a medium saucepan, bring the water to a boil. Add the salt, stir, and then add the rinsed and drained rice. Stir with a fork. \n");
+                 printf("4. Reduce the heat to low, cover, and let simmer for 20 minutes. Check it after 15 minutes to see if all the water has evaporated. If it has, the rice is ready. If not, replace the lid and let the rice simmer an additional 5 minutes.  \n");
+                 printf("5. Remove from the heat and fluff with a fork. Transfer to a plate and serve garnished with butter and parsley, if desired. \n");
+                 printf("\t\t\tHave A Nice Cooking :)");
+
+            break;
+
+    default: printf(" Coming Soon !!");
+
+}   
+    getch();
+    return 0;
+}

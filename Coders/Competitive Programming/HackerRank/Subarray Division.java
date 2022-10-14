8326f17b1132bd0fa9173886cc656ecda06d667a
Prax007-dot <73980804+Prax007-dot@users.noreply.github.com>
2022-10-15 01:21:44 +0530

    Create Subarray Division.java

---
+++ Coders/Competitive Programming/HackerRank/Subarray Division.java
@@ -0,0 +1,76 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'birthday' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY s
+     *  2. INTEGER d
+     *  3. INTEGER m
+     */
+
+    public static int birthday(List<Integer> s, int d, int m) {
+    // Write your code here
+    int p = m,c=0;
+    int n=s.size();
+    if (p == 1){
+        for(int i=0;i<n;i++){
+            int j = s.get(i);
+            if(j==d){
+                c+=1;
+            }
+        }
+    }else{
+       for(int i=0;i<n-(p-1);i++){
+            int sum = 0;
+            for(int j=0;j<p;j++){
+                sum = sum + s.get(i+j);
+            }
+            if(sum == d){c+=1;}
+        } 
+    }
+    return c;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> s = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int d = Integer.parseInt(firstMultipleInput[0]);
+
+        int m = Integer.parseInt(firstMultipleInput[1]);
+
+        int result = Result.birthday(s, d, m);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+ 

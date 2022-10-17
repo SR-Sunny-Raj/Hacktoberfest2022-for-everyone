91b233cf4843007ba5d47c303aacdc6b4be2726e
vibhumtri15 <115901960+vibhumtri15@users.noreply.github.com>
2022-10-18 01:20:27 +0530

    Create Min Heap.cpp

---
+++ Coders/DSA Concepts/Min Heap.cpp
@@ -0,0 +1,24 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main ()
+{
+	// Creates a max heap
+	priority_queue <int> pq;
+	pq.push(5);
+	pq.push(1);
+	pq.push(10);
+	pq.push(30);
+	pq.push(20);
+
+	// One by one extract items from max heap
+	while (pq.empty() == false)
+	{
+		cout << pq.top() << " ";
+		pq.pop();
+	}
+
+	return 0;
+}

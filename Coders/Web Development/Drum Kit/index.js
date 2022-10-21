348d98ec6fbdd185a4f736510630605bc075b8ed
curious-vv1 <96409335+curious-vv1@users.noreply.github.com>
2022-10-21 17:08:00 +0530

    A drum kit web project

---
+++ Coders/Web Development/Drum Kit/index.js
@@ -0,0 +1,65 @@
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+
+    document.querySelectorAll("button")[i].addEventListener("click", function () {
+        // this.style.color="green";
+        var buttonInnerHtml = this.innerHTML;
+        makeSound(buttonInnerHtml);
+
+        buttonAnimation(buttonInnerHtml);
+
+        // var audio = new Audio('sounds/tom-1.mp3');
+        // audio.play();
+    });
+}
+document.addEventListener("keydown", function (event) {          //---event is a variable
+    makeSound(event.key);
+
+    buttonAnimation(event.key);
+    // alert("key was pressed");
+});
+
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            var tom1 = new Audio('sounds/tom-1.mp3');
+            tom1.play();
+            break;
+        case "a":
+            var tom2 = new Audio('sounds/tom-2.mp3');
+            tom2.play();
+            break;
+        case "s":
+            var tom3 = new Audio('sounds/tom-3.mp3');
+            tom3.play();
+            break;
+        case "d":
+            var tom4 = new Audio('sounds/tom-4.mp3');
+            tom4.play();
+            break;
+        case "j":
+            var snare = new Audio('sounds/snare.mp3');
+            snare.play();
+            break;
+        case "k":
+            var crash = new Audio('sounds/crash.mp3');
+            crash.play();
+            break;
+        case "l":
+            var kick = new Audio('sounds/kick-bass.mp3');
+            kick.play();
+            break;
+        default:
+            console.log(buttonInnerHtml);
+            break;
+    }
+}
+
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function() {
+        activeButton.classList.remove("pressed");
+    },100
+    );
+}
